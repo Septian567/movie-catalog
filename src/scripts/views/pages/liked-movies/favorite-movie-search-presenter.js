62a2042a90bad531c5a25f7b38e503523573b9d2
Septian Dika Maulana <C2318G2774@bangkit.academy>
2025-01-02 10:39:01 +0700

    .

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -5,18 +5,22 @@ class FavoriteMovieSearchPresenter {
   }
 
   _listenToSearchRequestByUser() {
-    this._queryElement = document.getElementById('query');
-    this._queryElement.addEventListener('change', (event) => {
+    this._queryElement = document.getElementById("query");
+    this._queryElement.addEventListener("change", (event) => {
       this._searchMovies(event.target.value);
     });
   }
 
   async _searchMovies(latestQuery) {
-    this._latestQuery = latestQuery;
-    // this._favoriteMovies.searchMovies(this.latestQuery);
-    const foundMovies = await this._favoriteMovies.searchMovies(
-      this.latestQuery,
-    );
+    this._latestQuery = latestQuery.trim();
+
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
+
     this._showFoundMovies(foundMovies);
   }
 
@@ -24,19 +28,20 @@ class FavoriteMovieSearchPresenter {
   _showFoundMovies(movies) {
     console.log(movies);
     const html = movies.reduce(
-      (carry, movie) => carry.concat(`
+      (carry, movie) =>
+        carry.concat(`
          <li class="movie">
-          <span class="movie__title">${movie.title || '-'}</span>
+          <span class="movie__title">${movie.title || "-"}</span>
         </li>
         `),
-      '',
+      ""
     );
 
-    document.querySelector('.movies').innerHTML = html;
+    document.querySelector(".movies").innerHTML = html;
 
     document
-      .getElementById('movie-search-container')
-      .dispatchEvent(new Event('movies:searched:updated'));
+      .getElementById("movie-search-container")
+      .dispatchEvent(new Event("movies:searched:updated"));
   }
 
   get latestQuery() {
--- tests/favoriteMovieSearch.test.js
+++ tests/favoriteMovieSearch.test.js
@@ -1,11 +1,10 @@
-/*eslint-disable*/
-
 import { spyOn } from "jest-mock";
 import FavoriteMovieSearchPresenter from "../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter";
 import FavoriteMovieIdb from "../src/scripts/data/favorite-movie-idb";
 
 describe("Searching movies", () => {
   let presenter;
+  let favoriteMovies;
 
   const searchMovies = (query) => {
     const queryElement = document.getElementById("query");
@@ -27,9 +26,12 @@ describe("Searching movies", () => {
   };
 
   const constructPresenter = () => {
-    spyOn(FavoriteMovieIdb, "searchMovies");
+    favoriteMovies = {
+      getAllMovies: jest.fn(),
+      searchMovies: jest.fn(),
+    };
     presenter = new FavoriteMovieSearchPresenter({
-      favoriteMovies: FavoriteMovieIdb,
+      favoriteMovies,
     });
   };
 
@@ -38,121 +40,152 @@ describe("Searching movies", () => {
     constructPresenter();
   });
 
-  it("should be able to capture the query typed by the user", () => {
-    FavoriteMovieIdb.searchMovies.mockImplementation(() => []);
+  describe("when query is not empty", () => {
+    it("should be able to capture the query typed by the user", () => {
+      favoriteMovies.searchMovies.mockImplementation(() => []);
 
-    searchMovies("film a");
+      searchMovies("film a");
 
-    expect(presenter.latestQuery).toEqual("film a");
-  });
-
-  it("should ask the model to search for liked movies", () => {
-    FavoriteMovieIdb.searchMovies.mockImplementation(() => []);
-
-    searchMovies("film a");
-
-    expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith("film a");
-  });
-
-  it("should show the found movies", () => {
-    presenter._showFoundMovies([{ id: 1 }]);
-    expect(document.querySelectorAll(".movie").length).toEqual(1);
-
-    presenter._showFoundMovies([
-      {
-        id: 1,
-        title: "Satu",
-      },
-      {
-        id: 2,
-        title: "dua",
-      },
-    ]);
-
-    expect(document.querySelectorAll(".movie").length).toEqual(2);
-  });
-
-  it("should show the title of the found movies", () => {
-    presenter._showFoundMovies([
-      {
-        id: 1,
-        title: "Satu",
-      },
-    ]);
-    expect(
-      document.querySelectorAll(".movie__title").item(0).textContent
-    ).toEqual("Satu");
-
-    presenter._showFoundMovies([
-      {
-        id: 1,
-        title: "Satu",
-      },
-      {
-        id: 2,
-        title: "Dua",
-      },
-    ]);
-
-    const movieTitles = document.querySelectorAll(".movie__title");
-
-    expect(movieTitles.item(0).textContent).toEqual("Satu");
-    expect(movieTitles.item(1).textContent).toEqual("Dua");
-  });
-
-  it("should show - for found movie without title", () => {
-    presenter._showFoundMovies([{ id: 1 }]);
-    expect(
-      document.querySelectorAll(".movie__title").item(0).textContent
-    ).toEqual("-");
-  });
-
-  it("should show the movies found by Favorite Movies", (done) => {
-    document
-      .getElementById("movie-search-container")
-      .addEventListener("movies:searched:updated", () => {
-        expect(document.querySelectorAll(".movie").length).toEqual(3);
-        done();
-      });
-    FavoriteMovieIdb.searchMovies.mockImplementation((query) => {
-      if (query === "film a") {
-        return [
-          { id: 111, title: "film abc" },
-          { id: 222, title: "ada juga film abcde" },
-          { id: 333, title: "ini juga boleh film a" },
-        ];
-      }
-      return [];
-    });
-    searchMovies("film a");
-
-    // expect(document.querySelectorAll(".movie").length).toEqual(3);
-  });
-
-  it("should show the name of the movies found by Favorite Movies", (done) => {
-    document
-      .getElementById("movie-search-container")
-      .addEventListener("movies:searched:updated", () => {
-        const movieTitles = document.querySelectorAll(".movie__title");
-        expect(movieTitles.item(0).textContent).toEqual("film abc");
-        expect(movieTitles.item(1).textContent).toEqual("ada juga film abcde");
-        expect(movieTitles.item(2).textContent).toEqual(
-          "ini juga boleh film a"
-        );
-        done();
-      });
-
-    FavoriteMovieIdb.searchMovies.mockImplementation((query) => {
-      if (query === "film a") {
-        return [
-          { id: 111, title: "film abc" },
-          { id: 222, title: "ada juga film abcde" },
-          { id: 333, title: "ini juga boleh film a" },
-        ];
-      }
-      return [];
+      expect(presenter.latestQuery).toEqual("film a");
     });
 
-    searchMovies("film a");
+    it("should ask the model to search for liked movies", () => {
+      favoriteMovies.searchMovies.mockImplementation(() => []);
+
+      searchMovies("film a");
+
+      expect(favoriteMovies.searchMovies).toHaveBeenCalledWith("film a");
+    });
+
+    it("should show the found movies", () => {
+      presenter._showFoundMovies([{ id: 1 }]);
+      expect(document.querySelectorAll(".movie").length).toEqual(1);
+
+      presenter._showFoundMovies([
+        {
+          id: 1,
+          title: "Satu",
+        },
+        {
+          id: 2,
+          title: "dua",
+        },
+      ]);
+
+      expect(document.querySelectorAll(".movie").length).toEqual(2);
+    });
+
+    it("should show the title of the found movies", () => {
+      presenter._showFoundMovies([
+        {
+          id: 1,
+          title: "Satu",
+        },
+      ]);
+      expect(
+        document.querySelectorAll(".movie__title").item(0).textContent
+      ).toEqual("Satu");
+
+      presenter._showFoundMovies([
+        {
+          id: 1,
+          title: "Satu",
+        },
+        {
+          id: 2,
+          title: "Dua",
+        },
+      ]);
+
+      const movieTitles = document.querySelectorAll(".movie__title");
+
+      expect(movieTitles.item(0).textContent).toEqual("Satu");
+      expect(movieTitles.item(1).textContent).toEqual("Dua");
+    });
+
+    it("should show - for found movie without title", () => {
+      presenter._showFoundMovies([{ id: 1 }]);
+      expect(
+        document.querySelectorAll(".movie__title").item(0).textContent
+      ).toEqual("-");
+    });
+
+    it("should show the movies found by Favorite Movies", (done) => {
+      document
+        .getElementById("movie-search-container")
+        .addEventListener("movies:searched:updated", () => {
+          expect(document.querySelectorAll(".movie").length).toEqual(3);
+
+          done();
+        });
+
+      favoriteMovies.searchMovies.mockImplementation((query) => {
+        if (query === "film a") {
+          return [
+            { id: 111, title: "film abc" },
+            { id: 222, title: "ada juga film abcde" },
+            { id: 333, title: "ini juga boleh film a" },
+          ];
+        }
+
+        return [];
+      });
+
+      searchMovies("film a");
+    });
+
+    it("should show the name of the movies found by Favorite Movies", (done) => {
+      document
+        .getElementById("movie-search-container")
+        .addEventListener("movies:searched:updated", () => {
+          const movieTitles = document.querySelectorAll(".movie__title");
+
+          expect(movieTitles.item(0).textContent).toEqual("film abc");
+          expect(movieTitles.item(1).textContent).toEqual(
+            "ada juga film abcde"
+          );
+          expect(movieTitles.item(2).textContent).toEqual(
+            "ini juga boleh film a"
+          );
+
+          done();
+        });
+
+      favoriteMovies.searchMovies.mockImplementation((query) => {
+        if (query === "film a") {
+          return [
+            { id: 111, title: "film abc" },
+            { id: 222, title: "ada juga film abcde" },
+            { id: 333, title: "ini juga boleh film a" },
+          ];
+        }
+
+        return [];
+      });
+
+      searchMovies("film a");
+    });
+  });
+
+  describe("When query is empty", () => {
+    it("should capture the query as empty", () => {
+      favoriteMovies.getAllMovies.mockImplementation(() => []);
+      searchMovies(" ");
+      expect(presenter.latestQuery.length).toEqual(0);
+
+      searchMovies("    ");
+      expect(presenter.latestQuery.length).toEqual(0);
+      searchMovies("");
+      expect(presenter.latestQuery.length).toEqual(0);
+      searchMovies("\t");
+      expect(presenter.latestQuery.length).toEqual(0);
+    });
+
+    it("should show all favorite movies", () => {
+      favoriteMovies.getAllMovies.mockImplementation(() => []);
+
+      searchMovies("    ");
+      expect(favoriteMovies.getAllMovies).toHaveBeenCalled();
+    });
   });
 });
