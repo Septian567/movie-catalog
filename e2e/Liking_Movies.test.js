84c35c1d8244b38279fd40bf3d16a53e3e096241
Septian Dika Maulana <C2318G2774@bangkit.academy>
2025-01-03 17:47:52 +0700

    .

--- e2e/Liking_Movies.test.js
+++ e2e/Liking_Movies.test.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Movies');
 
 Before(({ I }) => {
@@ -8,18 +10,23 @@ Scenario('showing empty liked movies', ({ I }) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 });
 
-Scenario('liking one movie', ({ I }) => {
+Scenario('liking one movie', async ({ I }) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
   I.amOnPage('/');
 
   I.seeElement('.movie__title a');
-  I.click(locate('.movie__title a').first());
+  const firstMovie = locate('.movie__title a').first();
+  const firstMovieTitle = await I.grabTextFrom(firstMovie);
+  I.click(firstMovie);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
   I.seeElement('.movie-item');
+  const likedMovieTitle = await I.grabTextFrom('.movie__title');
 
+  assert.strictEqual(firstMovieTitle, likedMovieTitle);
   pause();
 });
